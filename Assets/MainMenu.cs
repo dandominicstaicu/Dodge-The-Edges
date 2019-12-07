4208cb82c1cd04f18563a60753f6cc69b24cb914
GGalinGG2002 <alotdenis@gmail.com>
2019-12-07 22:11:57 +0200

    Clearing code and more stuff

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -7,8 +7,8 @@ public class MainMenu : MonoBehaviour
 {
    public void PlayGame()
    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-   }
+        Application.LoadLevel("Nivel1");
+    }
     public void QuitGame()
     {
         Debug.Log("QUIT!");
--- Assets/MobileHealthController.cs
+++ Assets/MobileHealthController.cs
@@ -16,7 +16,7 @@ public class MobileHealthController : MonoBehaviour
     {
         if (playerHealth < 0)
         {
-            Application.LoadLevel("Untitled");
+            Application.LoadLevel("Menu");
         }
     }
 }
