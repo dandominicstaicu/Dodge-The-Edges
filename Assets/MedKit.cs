b8e68dacdd015d2d293dcd4eb10d1167b624f341
GGalinGG2002 <alotdenis@gmail.com>
2019-12-07 20:10:43 +0200

    Included 2 levels, with a lot of things inside them

---
+++ Assets/MedKit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MedKit : MonoBehaviour
+{
+    [SerializeField] private float vDamage;
+    [SerializeField] private MobileHealthController healthController;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Damage();
+        }
+    }
+    void Damage()
+    {
+        healthController.playerHealth = healthController.playerHealth - vDamage;
+        healthController.UpdateHealth();
+        this.gameObject.SetActive(false);
+    }
+}
