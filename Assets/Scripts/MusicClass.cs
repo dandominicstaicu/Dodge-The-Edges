abf4bd87cdd8cabdca9731b0059428970d66cb74
Depressed <alotdenis@gmail.com>
2019-12-08 07:51:31 +0200

    Volume changer is now working

---
+++ Assets/Scripts/MusicClass.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class MusicClass : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayMusic()
+    {
+        if (_audioSource.isPlaying) return;
+        _audioSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        _audioSource.Stop();
+    }
+    // Reference to Audio Source component
+    private AudioSource audioSrc;
+
+    // Music volume variable that will be modified
+    // by dragging slider knob
+    private float musicVolume = 1f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        // Assign Audio Source component to control it
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Setting volume option of Audio Source to be equal to musicVolume
+        audioSrc.volume = musicVolume;
+    }
+
+    // Method that is called by slider game object
+    // This method takes vol value passed by slider
+    // and sets it as musicValue
+    public void SetVolume(float vol)
+    {
+        musicVolume = vol;
+    }
+}
